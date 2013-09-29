12346056d3259e393469017d35eb3b08242083e0
nolearning <wang.zhenbin@gmail.com>
2013-09-29 20:08:30 +0800

    commit changes.

--- TestJackson/src/com/learning/java/TestMain.java
+++ TestJackson/src/com/learning/java/TestMain.java
@@ -1,9 +1,14 @@
 package com.learning.java;
 
 import java.io.File;
+import java.util.ArrayList;
+
 
 import com.fasterxml.jackson.*;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
+
+
 
 public class TestMain {
 	private static class Package {
@@ -56,17 +61,120 @@ public class TestMain {
 		public void readJSONByDataBinding() {
 			try {
 				Package p1 = mapper.readValue(jsonFile, Package.class);
-				mapper.createObjectNode();
 				System.out.println(p1.childNodes[1].childNodes[0].name);
 			} catch(Exception e) {
 				System.err.println(e);
 			}
 		}
 		
-		public void readJSONbyTreeModel() {
+		public void readJSONByTreeModel() {
 			try {
-				JsonNode rootNode = mapper.readValue(jsonFile, JsonNode.class);
-				System.out.println(rootNode.get("childNodes").asText());
+				//JsonNode rootNode = mapper.readValue(jsonFile, JsonNode.class);
+				JsonNode rootNode = mapper.readTree(jsonFile);
+				System.out.println(rootNode.size());
+				System.out.println(rootNode.get("childNodes").size());
+				System.out.println(rootNode.get("childNodes").get(0).get("name"));
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+		}
+		
+		private class Stack {
+			private final ArrayList<Object> objects;
+			
+			public Stack() {
+				objects = new ArrayList<Object>();
+			}
+			
+			public int size() {
+				return objects.size();
+			}
+			public boolean push(Object o) {
+				return objects.add(o);
+			}
+			
+			public Object pop() {
+				if(size() == 0) 
+					return null;
+				int lastIndex = size() - 1;
+				Object o = objects.get(lastIndex);
+				objects.remove(lastIndex);
+				return o;
+			}
+			
+			public Object top() {
+				if(size() == 0) 
+					return null;
+				int lastIndex = size() - 1;
+				Object o = objects.get(lastIndex);
+				return o;
+			}
+		}
+		
+		class Node {
+			public String name;
+			public String id;
+			public ArrayList<Node> childNodes;
+		}
+		
+		class TopNode extends Node {
+			public String publisher;
+			public String version;
+			public String grade;
+			public String subject;
+			public int fasiculeOrder;
+			public String fasciculeName;
+		}
+		
+		public void readJSONByStreamAPI() {
+			try {
+				JsonParser jp = new JsonFactory().createParser(jsonFile);
+				JsonToken token = null;
+				Stack stack = new Stack();
+				Object curNode = null;
+				
+				for(;;) {
+					token = jp.nextToken();
+					if(token == null) return;
+					if(token == JsonToken.START_OBJECT) {
+						System.out.println("Token start object");
+						if(stack.size() == 0) {
+							curNode = new TopNode();
+						} else {
+							stack.push(curNode);
+							curNode = new Node();
+							if(stack.top() instanceof ArrayList<?>) {
+								((ArrayList<Node>)stack.top()).add((Node)curNode);
+							}
+						}
+						continue;
+					} else if (token == JsonToken.END_OBJECT) {
+						System.out.println("Token end object");
+						curNode = stack.pop();
+						continue;
+					} else if (token == JsonToken.START_ARRAY) {
+						((Node)curNode).childNodes = new ArrayList<Node>();
+						stack.push(curNode);
+						curNode 
+						System.out.println("Token start array");
+						continue;
+					} else if (token == JsonToken.END_ARRAY) {
+						System.out.println("Token end array");
+						continue;
+					} else if(jp.getCurrentName().equals("childNodes")){
+						continue;
+					}
+					
+					
+					String fieldName = jp.getCurrentName();
+					jp.nextToken();
+					if("name".equals(fieldName)) {
+						((Node)curNode).name = jp.getText();
+					} else if("id".equals(fieldName)){
+						((Node)curNode).id = jp.getText();
+					}
+				}
+				
 			} catch (Exception e) {
 				System.err.println(e);
 			}
@@ -78,7 +186,8 @@ public class TestMain {
 		
 		TestJackson tj = tm.new TestJackson("package.json");
 		///tj.readJSONByDataBinding();
-		tj.readJSONbyTreeModel();
+		//tj.readJSONByTreeModel();
+		tj.readJSONByStreamAPI();
 	}
 
 }
---
+++ TestJackson/src/com/learning/java/TestTypeRef.java
@@ -0,0 +1,65 @@
+package com.learning.java;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+abstract class TypeRef<T> {
+	private final Type type;
+	protected TypeRef() {
+		type = ((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+		System.out.println("Typeref is refering to object of" + type);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		return o instanceof TypeRef && ((TypeRef)o).type.equals(type);
+	}
+	
+	@Override
+	public int hashCode() {
+		return type.hashCode();
+	}
+}
+
+public class TestTypeRef {
+	public static <T> void getType(TypeRef<T> t) {
+		ParameterizedType p = (ParameterizedType) t.getClass()
+				.getGenericSuperclass();
+		System.out.println(t.getClass());
+		System.out.println(p);
+		System.out.println(p.getActualTypeArguments()[0]);
+	}
+	
+	public static <T> void testGernericList() {
+		Map<TypeRef<?>, Object> objects = new HashMap<TypeRef<?>, Object>();
+		TypeRef<ArrayList<T>> ref = new TypeRef<ArrayList<T>>(){};
+		objects.put(ref, new ArrayList<String>());
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<String> ls = (ArrayList<String>)objects.get(new TypeRef<ArrayList<T>>(){});
+		ls.add("ss");
+		System.out.println(ls.get(0));
+		@SuppressWarnings("unchecked")
+		ArrayList<Integer> lis = (ArrayList<Integer>)objects.get(new TypeRef<ArrayList<T>>(){});
+		lis.add(1);
+		for(String s : ls) {
+			System.out.println(s);
+		}
+	}
+	
+	public static void test() {
+		//Test for generic type erasure
+		ArrayList<Integer> intArr = new ArrayList<Integer>();
+		ArrayList<Float> floatArr = new ArrayList<Float>();
+		if(intArr.getClass() == floatArr.getClass())
+			System.out.println("generic type erasure is taking effect.");
+		System.out.println(intArr.getClass());
+		System.out.println(intArr.getClass().getGenericSuperclass());
+		getType(new TypeRef<ArrayList<Integer>>(){});
+		testGernericList();
+	}
+}
